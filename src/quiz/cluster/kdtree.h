43330e88afe2b9f0ac787208b6dd250ed88294ca
Joel-Milla <jalejandro.milla@outlook.com>
2025-02-20 00:43:03 +0800

    Implemnt the search function of the kdtree algoritm to search for the k closests clusters.

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -71,15 +71,63 @@ struct KdTree
 		// TODO: Fill in this function to insert a new point into the tree
 		// the function should create a new node and place correctly with in the root 
 		insert(root, point, id, 0);
+	}
 
+	void traverse(Node* &node, std::vector<float> target, float distanceTol, std::vector<int> &ids, int level) {
+		if (node == NULL) // return when is null, terminating decision of recursion
+			return;
 		
+		float target_x = target[0];
+		float target_y = target[1];
 
+		// define the boundaries of the box around the target
+		float minX = target_x - distanceTol;
+		float maxX = target_x + distanceTol;
+		float minY = target_y - distanceTol;
+		float maxY = target_y + distanceTol;
+
+		// the current value of the node
+		int curr_id = node->id;
+		float curr_x = node->point[0];
+		float curr_y = node->point[1];
+
+		// bool variables to know if current node is within boundaries
+		bool currx_within = minX <= curr_x && curr_x <= maxX;
+		bool curry_within = minY <= curr_y && curr_y <= maxY;
+		bool within_box = currx_within && curry_within;
+
+		// go left if max value is less than curr axes value. go right when min value is greater than curr axes value. 
+		int indx = level % 2;
+		bool go_left = (target[indx] + distanceTol) < node->point[indx];
+		bool go_right = (target[indx] - distanceTol) > node->point[indx];
+		
+		level += 1; // increase level for next call
+		
+		// Just need to check if is within box, to add it. If not, just omit and continue with code
+		if (within_box) {
+			float distance = sqrt( (target_x - curr_x)*(target_x - curr_x) + (target_y - curr_y)*(target_y - curr_y));
+			
+			// check if distance is within distance tolerance
+			if (distance <= distanceTol)
+				ids.push_back(curr_id);
+		}
+		
+		// Both of the conditionals are independent. Can occur that both get executed (when within boundaries) or just one of them
+
+		if (go_left || within_box) { // if only need to go left
+			traverse(node->left, target, distanceTol, ids, level);
+		} 
+
+		if (go_right || within_box) { // if only need to go and check right boundary
+			traverse(node->right, target, distanceTol, ids, level);
+		}
 	}
 
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		traverse(root, target, distanceTol, ids, 0);
 		return ids;
 	}
 	
